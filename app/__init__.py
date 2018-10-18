f5a749cf205dc0d339b26880dcd700442af2935a
Lamech Lwasampijja <llwasampijja@gmail.com>
2018-10-18 16:18:14 +0300

    Added a test module

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,28 @@ from flask import Flask, Blueprint, jsonify
 from app.models.sales import Sales
 from app.views.sales_view import get_sales
 from .utilities import bp, endpoints_list
+import os
 
 def create_app(config_filename):
     app = Flask(__name__)
     app.register_blueprint(get_sales)
+
+    # beginning of tests
+
+    if config_filename is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.update(config_filename)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # ending of test
     return app
 
 
---
+++ app/tests/test_sales.py
@@ -0,0 +1,6 @@
+def test_index(client, auth):
+    response = client.get('/')
+    # assert b"Log In" in response.data
+    # assert b"Register" in response.data
+    assert response.status_code == 200
+    assert client.get("/wrong-endpoint").status_code == 403
