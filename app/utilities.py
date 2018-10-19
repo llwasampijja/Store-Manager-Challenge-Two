c02580efda8873eabc8a579c309a0563aa3bbc24
Lamech Lwasampijja <llwasampijja@gmail.com>
2018-10-19 20:16:42 +0300

    Moved the admin auth decorator to the utilities module to enable the reuse of code

--- app/utilities.py
+++ app/utilities.py
@@ -1,6 +1,37 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from functools import wraps
+
 blueprint = Blueprint('api', __name__)
 endpoints_list = """
 <h2>Store Manager Challenge Two</h2>
               <a href='/api/v1/products'>Admin Add Product</a></li>
 """
+
+ACCESS = {
+    'user': 0,
+    'store_attendant': 1,
+    'admin': 2
+}
+
+# Test case
+user_role = ACCESS['admin']
+author = True
+
+def publisher_and_admin(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        if author or user_role == 2:
+            return fn(*args, **kwargs)
+        else:
+            return jsonify(msg='This feature is available to only the admin and the individual who created it!'), 403
+    return wrapper
+
+def admin_authorised(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        if user_role == 2:
+            return fn(*args, **kwargs)
+        else:
+            return jsonify(msg='This featuer is only available to people with admin rights!'), 403
+            
+    return wrapper
--- app/views/products_view.py
+++ app/views/products_view.py
@@ -1,31 +1,10 @@
 from app.models.products import Products
 from flask import Flask, jsonify, request
-from app.utilities import blueprint
+from app.utilities import blueprint, admin_authorised
 from functools import wraps
 
 app = Flask(__name__)
 products_obj = Products()
-
-ACCESS = {
-    'user': 0,
-    'store_attendant': 1,
-    'admin': 2
-}
-
-# Test case
-user_role = ACCESS['admin']
-author = True
-
-def admin_authorised(fn):
-    @wraps(fn)
-    def wrapper(*args, **kwargs):
-        role = user_role
-        if role == 2:
-            return fn(*args, **kwargs)
-        else:
-            return jsonify(msg='This featuer is only available to people with admin rights!'), 403
-            
-    return wrapper
     
 @blueprint.route('/products', methods=['GET'])
 def get_products():   
