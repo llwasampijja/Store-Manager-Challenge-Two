7152b6e5c4d09812c61ae39ef60b3f2ac194aab3
Lamech Lwasampijja <llwasampijja@gmail.com>
2018-10-19 19:40:14 +0300

    Restricted getting a single sale to author and admin

--- app/utilities.py
+++ app/utilities.py
@@ -1,6 +1,38 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from functools import wraps
+
 blueprint = Blueprint('api', __name__)
 endpoints_list = """
 <h2>Store Manager Challenge Two</h2>
               <li><a href='/api/v1/sales/4'>Get A single Sale Record of Id 4</a></li>
 """
+
+ACCESS = {
+    'user': 0,
+    'store_attendant': 1,
+    'admin': 2
+}
+
+# Test case
+user_role = ACCESS['store_attendant']
+author = True
+
+def publisher_and_admin(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        if author or user_role == 2:
+            return fn(*args, **kwargs)
+        else:
+            return jsonify(msg='This feature is available to only the admin and the individual who created it!'), 403
+    return wrapper
+
+def admin_authorised(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        role = user_role
+        if role == 2:
+            return fn(*args, **kwargs)
+        else:
+            return jsonify(msg='This featuer is only available to people with admin rights!'), 403
+            
+    return wrapper
--- app/views/sales_view.py
+++ app/views/sales_view.py
@@ -1,15 +1,20 @@
 from app.models.sales import Sales
 from flask import Flask, jsonify
-from app.utilities import blueprint
+from app.utilities import blueprint, ACCESS, publisher_and_admin, admin_authorised
+from functools import wraps
 
 app = Flask(__name__)
 sales_records = Sales()
+
+# user page restrications are located in utilities.py
     
 @blueprint.route('/sales', methods=['GET'])    
+@admin_authorised
 def get_sales():
     return jsonify(sales_records.get_all_sales())
 
 # GET a sale by its id
 @blueprint.route('/sales/<int:sale_index>' , methods=['GET'])
+@publisher_and_admin
 def get_a_product(sale_index):
     return jsonify(sales_records.get_single_sale (sale_index))
