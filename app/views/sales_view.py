ffe02bf56b7aed7880848b2d6c08f64f6358f87e
Lamech Lwasampijja <llwasampijja@gmail.com>
2018-10-19 12:12:47 +0300

    add unittest for get all products API endpoint

--- app/views/sales_view.py
+++ app/views/sales_view.py
@@ -1,10 +1,10 @@
 from app.models.sales import Sales
 from flask import Flask, jsonify
-from app.utilities import bp
+from app.utilities import blueprint
 
 app = Flask(__name__)
     
-@bp.route('/sales', methods=['GET'])    
+@blueprint.route('/sales', methods=['GET'])    
 def get_sales():   
     sales_records = Sales()
     return jsonify(sales_records.get_all_sales())
