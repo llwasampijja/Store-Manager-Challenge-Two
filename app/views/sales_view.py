930b89f35b328dbdeaae8f3fe90309d17dba5642
Lamech Lwasampijja <llwasampijja@gmail.com>
2018-10-19 19:50:39 +0300

    Restricted viewing all sales to only the admin

--- app/views/sales_view.py
+++ app/views/sales_view.py
@@ -1,10 +1,11 @@
 from app.models.sales import Sales
 from flask import Flask, jsonify
-from app.utilities import blueprint
+from app.utilities import blueprint, admin_authorised
 
 app = Flask(__name__)
     
-@blueprint.route('/sales', methods=['GET'])    
+@blueprint.route('/sales', methods=['GET'])
+@admin_authorised   
 def get_sales():   
     sales_records = Sales()
     return jsonify(sales_records.get_all_sales())
