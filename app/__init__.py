08c151660423fb7105abc8ad3f40dd8eab8edca9
Lamech Lwasampijja <llwasampijja@gmail.com>
2018-10-20 17:54:11 +0300

    Added landing page for all the links of the API endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,8 @@ from flask import Flask, Blueprint, jsonify
 from app.models.sales import Sales
 from app.views.sales_view import get_sales
 from app.views.products_view import get_products
-from .utilities import blueprint, endpoints_list
+from .utilities import blueprint
+from .landing_page import webpage
 import os
 
 def create_app(config_filename):
@@ -15,6 +16,6 @@ app = Flask(__name__)
 # Landing Page
 @blueprint.route("/", methods=["GET"])
 def index():
-    return endpoints_list
+    return webpage
 
 app.register_blueprint(blueprint, url_prefix='/api/v1')
