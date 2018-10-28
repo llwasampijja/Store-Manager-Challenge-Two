778de062f2b704d4e3ead2c1dfb97a289742a7a3
Lamech Lwasampijja <llwasampijja@gmail.com>
2018-10-28 17:21:06 +0300

    Add add category endpoint

--- app/views/pdt_category_view.py
+++ app/views/pdt_category_view.py
@@ -1,6 +1,7 @@
 from flask import Response, request, Blueprint
 import json
 from app.models.pdt_category_model import PdtCategory
+from app.utilities import create_id
 
 pdt_category_bp = Blueprint("pdt_category", __name__)
 
@@ -15,4 +16,13 @@ def get_all_product_categories():
 @pdt_category_bp.route("/categories/<int:category_id>", methods=["GET"])
 def get_category(category_id):
     response = Response(json.dumps(pdt_category_obj.get_category_by_id(category_id)), content_type="application/json", status=200)
+    return response
+
+@pdt_category_bp.route("/categories/add", methods=["POST"])
+def add_category():
+    request_data = request.get_json()
+    pdt_category_id = create_id(pdt_categories)
+    request_data.update({"pdt_category_id":pdt_category_id})
+    pdt_categories.append(request_data)
+    response = Response(json.dumps(pdt_categories), content_type="application/json", status=200)
     return response
